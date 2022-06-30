2b4a6e3cee5f3513fb3b6ac82e906e6f1725539e
vwerne <vwerne@gmail.com>
2022-06-30 13:59:51 +0200

    chore: change error to warning

--- core/src/hooks/useLayerManagement.js
+++ core/src/hooks/useLayerManagement.js
@@ -6,9 +6,12 @@ const useLayerManagement = () => {
 	const registerActiveLayer = useCallback((layer, layerType) => {
 		setActiveLayers((prevLayers) => {
 			if (prevLayers.some(({ type }) => type === layerType)) {
-				throw new Error(
+				// eslint-disable-next-line no-console
+				console.warn(
 					`There can be only one rendered layer of type ${layerType} in map`
 				);
+
+				return prevLayers;
 			}
 
 			return [...prevLayers, { type: layerType, data: layer }];
